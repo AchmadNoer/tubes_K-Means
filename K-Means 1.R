073df0a10af818a6e6a1f6fe773f30d99f53785b
AgentF <17523001@students.uii.ac.id>
2020-07-05 18:34:02 +0700

    Versi 1.0

---
+++ K-Means 1.R
@@ -0,0 +1,24 @@
+dataIkan <- read.csv("Ikan.csv", stringsAsFactors=TRUE, fileEncoding="UTF-8-BOM")
+head(dataIkan)
+summary(dataIkan)
+
+dataIkan.ciri <- dataIkan[, 6:7]
+dataIkan.jenis <- dataIkan[, "Species"]
+set.seed(62)
+hasilKMeans <- kmeans(dataIkan.ciri, 7)
+
+par(mfrow=c(1,2))
+plot(dataIkan.ciri, col = hasilKMeans$cluster, main="K-Means")
+plot(dataIkan.ciri, col = dataIkan.jenis, main="Original")
+
+hasilKMeans$tot.withinss
+
+dataIkan.ciri$cluster <- factor(hasilKMeans$cluster)
+centers <- as.data.frame(hasilKMeans$centers)
+
+library("ggplot2")
+ggplot() + 
+  geom_point(data = dataIkan.ciri, aes(x = Height, y = Width, color = cluster)) + 
+  geom_point(data = centers, aes(x = Height, y = Width, color = "Center"), 
+             size = 30, alpha = 0.2, show.legend = FALSE)
+  
